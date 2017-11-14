986fe3b7302e5f0c64c8fba7ce5ec9f4e17e65b8
Suzuki Takaya <sbs_suzuki@worksap.co.jp>
2017-11-14 17:03:13 +0900

    LEARN ch04

--- ch04/cross_entropy_error.py
+++ ch04/cross_entropy_error.py
@@ -2,4 +2,10 @@ import numpy as np
 
 def cross_entropy_error(y, t):
     delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+	y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + delta)) / batch_size
