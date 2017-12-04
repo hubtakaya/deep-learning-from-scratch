49c38e49eada2152809f617731a6817886d67f66
Takaya Suzuki <tk.yaaa7@gmail.com>
2017-12-04 23:53:29 +0900

    LEARN ch05

--- ch05/layers.py
+++ ch05/layers.py
@@ -7,16 +7,16 @@ class Relu:
 
     def forward(self, x):
         self.mask = (x <= 0)
-	out = x.copy()
-	out[self.mask] = 0
+        out = x.copy()
+        out[self.mask] = 0
 	
-	return out
+        return out
 
     def backward(self, dout):
         dout[self.mask] = 0
-	dx = dout
+        dx = dout
 
-	return dx
+        return dx
 
 class Sigmoid:
     def __init__(self):
@@ -24,61 +24,62 @@ class Sigmoid:
 
     def forward(self, x):
         out = 1 / (1 + np.exp(-x))
-	self.out = out
+        self.out = out
 
-	return out
+        return out
 
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
 
-	return dx
+        return dx
 
 class Affine:
-    def __init(self, W, b):
+    def __init__(self, W, b):
         self.W = W
-	self.b = b
+        self.b = b
 
-	self.x = None
-	self.original_x_shape = None
-	# 重み, バイアスパラメ-タの微分
+        self.x = None
+        self.original_x_shape = None
+        # 重み, バイアスパラメ-タの微分
         self.dW = None
-	self.db = None
+        self.db = None
 
     def forward(self, x):
         self.original_x_shape = x.shape
-	x = x.reshape(x.shape[0], -1)
-	self.x = x
+        x = x.reshape(x.shape[0], -1)
+        self.x = x
 
-	out = np.dot(self.x, self.W) + self.b
+        out = np.dot(self.x, self.W) + self.b
 
-	return out
+        return out
 
     def backward(self, dout):
         dx = np.dot(dout, self.W.T)
-	self.dW = np.dot(self.x.T, dout)
-	self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
 
-	dx = dx.reshape(*self.original_x_shape)
-	return dx
+        dx = dx.reshape(*self.original_x_shape)
+        return dx
 
 class SoftmaxWithLoss:
-    def __init(self):
+    def __init__(self):
         self.loss = None
         self.y = None
-	self.t = None
+        self.t = None
 
     def forward(self, x, t):
         self.t = t
-	self.y = softmax(x)
-	self.loss = cross_entropy_error(self.y, self.t)
+        # self.y = softmax(x)
+        self.y = x
+        self.loss = cross_entropy_error(self.y, self.t)
 
-	return self.loss
+        return self.loss
 
     def backward(self, dout=1):
         batch_size = self.t.shape[0]
-	dx = (self.y - self.t) / batch_size
+        dx = (self.y - self.t) / batch_size
 
-	return dx
+        return dx
 
 
 
--- ch05/two_layer_net.py
+++ ch05/two_layer_net.py
@@ -1,43 +1,40 @@
+# coding: utf-8
 import sys, os
 sys.path.append(os.pardir)
 import numpy as np
-from common layers import *
+from layers import *
 from common.gradient import numerical_gradient
 from collections import OrderedDict
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         self.params = {}
-        self.params['W'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init.std * np.random.randn(hidden_size, output_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
-	self.layers = OrderedDict()
-	self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
-	self.layers['Relu1'] = Relu()
-	self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
 
-	self.lastLayer = SoftmaxWithLoss()
+        self.lastLayer = SoftmaxWithLoss()
 
     def predict(self, x):
         for layer in self.layers.values():
-	    x = layer.forward(x)
+            x = layer.forward(x)
 
-	return x
-
-    def loss(self, x, t):
-        y = self.predict(x)
-	return self.lastLayer.forward(y, t)
+        return x
 
     def loss(self, x, t):
         y = self.predict(x)
-	print("y = self.predict: ", y)
-	yy = np.argmax(y, axis=0)
-	print("y = self.predict: ", yy)
-	y = np.argmax(y, axis=1)
-	print("y = np.argmax(y, axis=1)", y)
-	if t.ndim != 1 : t = np.argmax(t, axis=1)
+        return self.lastLayer.forward(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        if t.ndim != 1 : t = np.argmax(t, axis=1)
         accuracy = np.sum(y == t) / float(x.shape[0])
 
         return accuracy
@@ -45,7 +42,7 @@ class TwoLayerNet:
     def numerical_gradient(self, x, t):
         loss_W = lambda W: self.loss(x, t)
 
-	grads = {}
+        grads = {}
         grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
         grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
         grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
@@ -56,15 +53,15 @@ class TwoLayerNet:
     def gradient(self, x, t):
         self.loss(x, t)
 
-	dout = 1
-	dout = self.lastLayer.backward(dout)
+        dout = 1
+        dout = self.lastLayer.backward(dout)
 
-	layers = list(self.layers.values())
-	layers.reverse()
-	for layer in layers:
-	    dout = layer.backward(dout)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
 	
-	grads = {}
+        grads = {}
         grads['W1'] = self.layers['Affine1'].dW 
         grads['b1'] = self.layers['Affine1'].db 
         grads['W2'] = self.layers['Affine2'].dW 
